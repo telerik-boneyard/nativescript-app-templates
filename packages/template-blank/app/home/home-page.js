914565b37c5638384f807654bcff27332eccc09d
rosen-vladimirov <rosen.vladimirov@progress.com>
2020-05-22 16:16:27 +0300

    chore: remove templates

--- packages/template-blank/app/home/home-page.js
+++
@@ -1,8 +0,0 @@
-const HomeViewModel = require("./home-view-model");
-
-function onNavigatingTo(args) {
-    const page = args.object;
-    page.bindingContext = new HomeViewModel();
-}
-
-exports.onNavigatingTo = onNavigatingTo;
